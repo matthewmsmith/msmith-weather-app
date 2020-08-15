909f85c9e69753c80df1380ab7e6a6ba4151be38
Matthew Smith <58535096+matthewmsmith@users.noreply.github.com>
2020-08-15 01:45:57 -0400

    Search bar
    Get weather based on location

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-const locationTimeZone = document.querySelector(".location-timezone");
-const temperatureDegree = document.querySelector(".temperature-degree");
-const temperatureDescription = document.querySelector(".temperature-description");
-const icon = document.querySelector('#weather-icon')
-const inputValue = document.querySelector(".temperature-input-value");
-let temperature = document.querySelector('.temperature');
-const button = document.querySelector(".button");
+let wrapper = document.querySelector(".wrapper");
+
+const locationName = document.querySelector(".location-name");
+
+const icon = document.querySelector("#weather-icon");
+const temperatureDescription = document.querySelector(
+  ".temperature-description"
+);
+
+const degree = document.querySelector(".degree");
+
+const locationInput = document.querySelector(".location-input");
+const button = document.querySelector(".location-submit");
+
 let lon;
 let lat;
 
@@ -14,45 +21,74 @@ window.addEventListener("load", () => {
       lon = position.coords.longitude;
       lat = position.coords.latitude;
 
-      const myLocationWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=fec17107f5bf9d5ec947195fcd401778`;
+      const myLocationWeatherApi = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=fec17107f5bf9d5ec947195fcd401778`;
 
-      fetch(myLocationWeather)
+      fetch(myLocationWeatherApi)
         .then((response) => {
           return response.json();
         })
         .then((data) => {
           const name = data.name;
           const { temp } = data.main;
-          const { id, description } = data.weather[0];
-        
-
-          locationTimeZone.textContent = name;
-          temperatureDegree.textContent = Math.round(temp - 273.15) * 9/5 + 32;
+          const {id, description } = data.weather[0];
+          let {icon} = data.weather[0]
+          locationName.textContent = name;
+          degree.textContent = (Math.round(temp - 273.15) * 9) / 5 + 32;
           temperatureDescription.textContent = description;
- 
-         
-          if (id < 250) {
-            icon.src = "./icons/thunderstorm.svg";
-            temperature.style.background = 'linear-gradient(rgba(82, 50, 134, 0.993), rgb(129, 106, 150), rgba(0, 0, 0, 0.849))';
-          } else if (id < 350) {
-            icon.src = "./icons/drizzle.svg";
-            temperature.style.background = 'linear-gradient(rgb(15, 18, 29), rgb(201, 198, 198), rgb(50, 49, 53, 0.849))';
-          } else if (id < 550) {
-            icon.src = "./icons/rain.svg";
-           temperature.style.background = 'linear-gradient(rgb(15, 18, 29), rgb(201, 198, 198), rgb(50, 49, 53, 0.849))';
-          } else if (id < 650) {
-            icon.src = "./icons/snow.svg";
-            temperature.style.background = 'linear-gradient( rgb(245, 244, 244), rgb(201, 198, 198), rgb(245, 243, 243))';
-          }  else if (id === 800) {
-            icon.src = "./icons/clear.svg";
-            temperature.style.background = 'linear-gradient(rgb(72, 140, 218), rgb(31, 95, 235), rgb(255, 255, 255))';
-          } else if (id === 802) {
-            icon.src = "./icons/clouds.svg";
-           temperature.style.background = 'linear-gradient(rgb(90, 89, 89), rgb(158, 154, 154), rgb(185, 185, 185))';
-          }
-        
+          changeBackgroundColor(id, icon);
+
           console.log(data);
         });
     });
   }
 });
+
+button.addEventListener('click', () => {
+      const searchLocationApi = `https://api.openweathermap.org/data/2.5/weather?q=${locationInput.value}&appid=fec17107f5bf9d5ec947195fcd401778`;
+
+      fetch(searchLocationApi) 
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          const name = data.name;
+          const { temp } = data.main;
+          const {description } = data.weather[0];
+          const {id} = data.weather[0]
+          let {icon} = data.weather[0]
+          locationName.textContent = name;
+          degree.textContent = (Math.round(temp - 273.15) * 9) / 5 + 32;
+          temperatureDescription.textContent = description;
+          changeBackgroundColor(id, icon);
+         
+        })
+      
+      });
+
+      function changeBackgroundColor(id) {
+        if (id < 250) {
+          icon.src = "./icons/thunderstorm.svg";
+          wrapper.style.background =
+            "linear-gradient(rgba(82, 50, 134, 0.993), rgb(129, 106, 150), rgba(0, 0, 0, 0.849))";
+        } else if (id < 350) {
+          icon.src = "./icons/drizzle.svg";
+          wrapper.style.background =
+            "linear-gradient(rgb(15, 18, 29), rgb(201, 198, 198), rgb(50, 49, 53, 0.849))";
+        } else if (id < 550) {
+          icon.src = "./icons/rain.svg";
+          wrapper.style.background =
+            "linear-gradient(rgb(15, 18, 29), rgb(201, 198, 198), rgb(50, 49, 53, 0.849))";
+        } else if (id < 650) {
+          icon.src = "./icons/snow.svg";
+          wrapper.style.background =
+            "linear-gradient( rgb(245, 244, 244), rgb(201, 198, 198), rgb(245, 243, 243))";
+        } else if (id === 800) {
+          icon.src = "./icons/clear.svg";
+          wrapper.style.background =
+            "linear-gradient(rgb(72, 140, 218), rgb(31, 95, 235), rgb(255, 255, 255))";
+        } else if (id === 802) {
+          icon.src = "./icons/clouds.svg";
+          wrapper.style.background =
+            "linear-gradient(rgb(90, 89, 89), rgb(158, 154, 154), rgb(185, 185, 185))";
+        }
+      }
